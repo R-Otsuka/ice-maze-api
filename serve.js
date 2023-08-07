598457521de3aadcffa7b6f99d7256cee948f18b
R-Otsuka <yakugozy@gmail.com>
2023-08-07 10:57:30 +0900

    responseの形式を変換

--- serve.js
+++ serve.js
@@ -19,14 +19,32 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// 一旦不変にする
+const FLOOR_LENGTH = 20;
+const STONE_COUNT = 12;
+
 app.get('/floor', (req, res) => {
-  // ここに氷の迷路アルゴリズムを作る。
-  const floorLength = 10;
-  const map = _.map(new Array(floorLength), (index) => {
-    return _.map(new Array(floorLength), (val) => Math.round(Math.random()));
+  // 可変にする
+  const start = { x: 1, y: 0 };
+  const goal = { x: 20, y: 21 };
+  const score = 0;
+  const map = _.map(new Array(FLOOR_LENGTH + 2), (val, y) => {
+    if (y === 0 || y === FLOOR_LENGTH + 1) {
+      return _.map(new Array(FLOOR_LENGTH + 2), (_val, x) => {
+        if ((start.x === x && start.y === y) || (goal.x === x && goal.y === y)) {
+          return 1;
+        }
+        return 0;
+      });
+    }
+    return _.map(new Array(FLOOR_LENGTH + 2), (_val, x) => {
+      if ((start.x === x && start.y === y) || (goal.x === x && goal.y === y)) {
+        return 1;
+      }
+      return Math.round(Math.random()) + 1;
+    });
   });
-  console.log(map);
-  res.json(map);
+  res.json({ map, start, goal, stone: STONE_COUNT, length: FLOOR_LENGTH, score });
 });
 
 app.use(router);
