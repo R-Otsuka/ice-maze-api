4d8f065132e1b9c582d62c101606759b73e3801d
R-Otsuka <yakugozy@gmail.com>
2023-08-29 09:15:22 +0900

    迷路の難易度変更用エンドポイントの雛形作成

--- serve.js
+++ serve.js
@@ -43,22 +43,11 @@ app.get('/floor', (req, res) => {
   res.json({ map, start, goal, stone: STONE_COUNT, length: FLOOR_LENGTH, score, min_steps: minSteps, path: goalPath });
 });
 
-app.put('/floor', (req, res) => {
-  // 可変にする
-  const current = {
-    // const start = { x: 1, y: 0 };
-    // const goal = { x: 20, y: 21 };
-    // let map = [];
-    // let score = 0;
-    // let minSteps = 0;
-    // let goalPath = [];
-  }
-  let score = 0;
+app.post('/floor/evolve', (req, res) => {
   while (score > current.score) {
     // mapの遺伝的合成と評価、scoreの更新を繰り返す。
     // この生成過程をできれば画面表示したい。
   }
-  res.json({ map, start, goal, stone: STONE_COUNT, length: FLOOR_LENGTH, score, min_steps: minSteps, path: goalPath });
 });
 
 app.use(router);
